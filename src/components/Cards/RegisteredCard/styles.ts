4f769c78adeac1fbe1bba030ef96e46222ee0aea
gufernandess <gustavofer14@alu.ufc.br>
2022-09-20 22:38:46 -0300

    IDC22-08

---
+++ src/components/Cards/RegisteredCard/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const ContainerRegisteredCard = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;`
+
+export const RegisteredHeaderText = styled.h1`
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 600;
+font-size: 2rem;
+line-height: 47px;
+margin-top: 25px;
+color: #323232;
+  @media (max-width: 1366px) {
+    font-size: 2.4rem;
+  }
+  @media (max-width: 1280px) {
+    font-size: 2rem;
+  }
+  @media (max-width: 1150px) {
+    font-size: 1.9rem;
+  }
+  @media (max-width: 1080px) {
+    font-size: 1.8rem;
+  }
+  @media (max-width: 846px) {
+    font-size: 1.7rem;
+  }
+  @media (max-width: 720px) {
+    font-size: 1.55rem;
+  }`
+
+  export const RegisteredNavBar = styled.div`
+  display: flex;
+  border-bottom: 1px solid var(--textColor100);
+  flex-direction: row;
+  gap: 75px;
+  justify-content: flex-start;
+  margin-top: 25px;`
+
+  export const RegisteredButton = styled.button`
+  background: none;
+  border: none;
+  border-bottom: 1px solid transparent;
+  color: var(--textGray);
+  display: flex;
+  font-family: 'Poppins';
+  font-size: 21px;
+  font-weight: 400;
+  padding: 10px 0;
+  width: 110px;`
